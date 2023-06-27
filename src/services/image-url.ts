f798a586efbf0a3ef023d29d837a8d0a064b6b20
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-06-27 18:14:58 +0600

    optimize image

---
+++ src/services/image-url.ts
@@ -0,0 +1,11 @@
+const getCroppedImageUrl = (url: string) => {
+  if (!url)
+    return "https://raw.githubusercontent.com/mosh-hamedani/game-hub/main/src/assets/no-image-placeholder.webp";
+
+  const target = "media/";
+  const index = url?.indexOf(target) + target?.length;
+
+  return url?.slice(0, index) + "crop/600/400/" + url?.slice(index);
+};
+
+export default getCroppedImageUrl;
