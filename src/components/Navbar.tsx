30bb4ac666e8d5e0e2e6b1b9ecd1e65f264d484b
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-06-27 16:19:39 +0600

    build the dark mode switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack padding="10px" justifyContent="space-between">
       <Image
         boxSize="60px"
         src="https://game-hub-phi.vercel.app/assets/logo-ff4914e6.webp"
       />
       <Text>Navbar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
