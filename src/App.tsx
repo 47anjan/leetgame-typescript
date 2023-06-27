429d63006b3b3a30ecfb8c4d8cbdffec295b3d68
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-06-27 16:07:29 +0600

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button>HEllo</Button>
+      <Grid
+        gridTemplateAreas={{
+          base: `"nav""main"`,
+          lg: `"nav nav""aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
+      >
+        <GridItem bg="gold" area={"nav"}>
+          <h1>Nav</h1>
+        </GridItem>
+        <Show above="lg">
+          <GridItem
+            height={"fit-content"}
+            position={"sticky"}
+            top={0}
+            area={"aside"}
+            paddingX={5}
+            bg="coral"
+          >
+            <h1>Sidebar</h1>
+          </GridItem>
+        </Show>
+        <GridItem bg="blue" area={"main"}>
+          <h1>Main</h1>
+        </GridItem>
+      </Grid>
     </>
   );
 }
